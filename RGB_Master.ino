5f04e1a5e4ca7277bbb578f61ee85e2c0b2715da
Tim301 <45065016+Tim301@users.noreply.github.com>
2020-12-27 23:35:01 +0100

    Update RGB_Master.ino
    Testing Arduino Json

--- RGB_Master.ino
+++ RGB_Master.ino
@@ -1,17 +1,27 @@
-String inputString = "";         // a string to hold incoming data
+#include <ArduinoJson.h>
+#include <Wire.h>
+#include <Adafruit_TCS34725.h>
+
+  String input = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
+JsonObject obj;
 
 void setup() {
   // initialize serial:
   Serial.begin(9600);
   // reserve 200 bytes for the inputString:
   randomSeed(analogRead(0));
+  DynamicJsonDocument doc(1024);
+  int randNumber = random(300);
+  int randNumber2 = random(255);
+  input ="{\"1\":{\"R\": 55,\"G\":155,\"B\":255,\"W\":"+ String(randNumber)+",\"Bat\":59},\"2\":{\"R\": 50,\"G\":150,\"B\":250,\"W\":"+ String(randNumber2)+",\"Bat\":99}}";
+  deserializeJson(doc, input);
+  obj = doc.as<JsonObject>();
 }
 
 void loop() {
-  int randNumber = random(300);
-  delay(10);
-  int randNumber2 = random(255);
-  Serial.println("!{\"1\":{\"R\": 55,\"G\":155,\"B\":255,\"W\":"+ String(randNumber)+",\"Bat\":59},\"2\":{\"R\": 50,\"G\":150,\"B\":250,\"W\":"+ String(randNumber2)+",\"Bat\":99}}¡");
+  Serial.println("!"+input+"¡");
+  //int test = obj["1"]["R"];
+  //Serial.println(test);
   delay(100);
 }
